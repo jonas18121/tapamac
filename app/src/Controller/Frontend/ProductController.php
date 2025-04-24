d46a747eb3cba6791f27cdee374863d774c578cf
jonas18121 <jonasxtrm971@gmail.com>
2025-04-24 19:58:34 +0200

    [Add] : Add CRUD to Frontend/product with surcharge

---
+++ app/src/Controller/Frontend/ProductController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Entity\User;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Manager\ProductManager;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class ProductController extends AbstractController
+{
+    #[Route(
+        'product', 
+        name: 'app_product_list'
+    )]
+    public function list(
+        ProductRepository $productRepository, 
+        Request $request
+    ): Response
+    {
+        return $this->render('frontend/pages/product/list.html.twig', [
+            'pagination' => $productRepository->findPaginationList($request->query->getInt('page', 1), 'product', 15)
+        ]);
+    }
+
+    #[Route(
+        'product/create', 
+        name: 'app_product_create'
+    )]
+    public function create(
+        Request $request,
+        ProductManager $productManager
+    ): Response
+    {
+        /** @var User|null */
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productManager->create($product, $user);
+
+            // add flash
+            $this->addFlash(
+                'success',
+                'Le produit a bien été créée'
+            );
+
+            // Redirection
+            return $this->redirectToRoute('app_product_list');
+        }
+
+        return $this->render('frontend/pages/product/create.html.twig', [
+            'formProduct' => $form->createView(),
+        ]);
+    }
+
+    #[Route(
+        'product/delete/{id}', 
+        name: 'app_product_delete', 
+        requirements: ["id" => "\d+"],
+        methods: ["DELETE"]
+    )]
+    public function delete(
+        Product $product,
+        ProductManager $productManager,
+        Request $request
+    ): Response 
+    {
+        /** @var User|null */
+        $user = $this->getUser();
+
+        /** @var string|null */
+        $token = $request->get('_token');
+
+        if (!$user || null === $token) {
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        // $this->denyAccessUnlessGranted('delete', $product);
+
+        if ($this->isCsrfTokenValid('delete', $token)) {
+            $productManager->delete($product);
+        }
+
+        // add flash
+        $this->addFlash(
+            'success',
+            'Le produit a bien été supprimée'
+        );
+
+         // Redirection
+        return $this->redirectToRoute('app_product_list');
+    }
+
+    #[Route(
+        'product/update/{id}', 
+        name: 'app_product_update', 
+        requirements: ["id" => "\d+"],
+        methods: ["GET", "PUT"]
+    )]
+    public function update(
+        Product $product,
+        Request $request,
+        ProductManager $productManager
+    ): Response 
+    {
+        /** @var User|null */
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        // voter
+        // $this->denyAccessUnlessGranted('edit', $storageSpace);
+
+        $form = $this->createForm(ProductType::class, $product, ['method' => 'PUT']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productManager->update($product);
+
+            // add flash
+            $this->addFlash(
+                'success',
+                'Le produit a bien été modifié'
+            );
+
+            // Redirection
+            return $this->redirectToRoute('app_product_list');
+        }
+
+        return $this->render('frontend/pages/product/update.html.twig', [
+            'formProduct' => $form->createView(),
+        ]);
+    }
+
+    #[Route(
+        'product/{id}', 
+        name: 'app_product_detail', 
+        requirements: ["id" => "\d+"],
+        methods: ["GET"]
+    )]
+    public function detail(
+        Product $product
+    ): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        // $this->denyAccessUnlessGranted('show', $product);
+
+        return $this->render('frontend/pages/product/detail.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
