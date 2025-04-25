743436412133816808206fe6fdba429a7febac75
jonas18121 <jonasxtrm971@gmail.com>
2025-04-25 18:48:46 +0200

    [Add] : Add Read and Upate to Frontend/User

--- app/src/Form/UserType.php
+++ app/src/Form/UserType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
@@ -19,14 +18,22 @@ class UserType extends AbstractType
             ->add('firstName')
             ->add('lastName')
             ->add('phoneNumber')
-            ->add('isVerified', CheckboxType::class)
         ;
+
+        if (
+            array_key_exists('window_user',$options) 
+            && isset($options['window_user']) 
+            && 'backend' === $options['window_user']
+        ) {
+            $builder->add('isVerified', CheckboxType::class);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'window_user' => null
         ]);
     }
 }
